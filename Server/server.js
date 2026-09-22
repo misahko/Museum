f78a7b1e406101348d591df7e670aac2a610c02b
misahko <and-ria@ukr.net>
2026-09-23 01:41:43 +0300

    add guard and derive

--- Server/server.js
+++ Server/server.js
@@ -3,28 +3,40 @@ import { jwt } from "@elysiajs/jwt";
 import { cors } from "@elysiajs/cors";
 import bcrypt from "bcryptjs";
 
-const db = {
-  findByLogin: (login) => users.find((u) => u.login === login),
-  findBiId: (userId) => users.find((u) => u.id === userId),
-  add: (user) => {
-    users.push(user);
-    console.log(`created ${user}`);
-  },
-  getPersonalTests: (userId) => {
-    return personalTests;
-  },
-  getLibraryTests: () => {
-    return tests;
-  },
-  getStudents: (userId) => {
-    return [];
-  },
-};
+const db = {};
 
-const app = new Elysia().use(cors()).use(
-  jwt({
-    name: "jwt",
-    secret: process.env.JWT_SEVRET ?? "dev-secret-change-me",
-    exp: "7d",
-  }).post(),
-);
+const app = new Elysia()
+  .use(cors())
+  .use(
+    jwt({
+      name: "jwt",
+      secret: process.env.JWT_SEVRET ?? "dev-secret-change-me",
+      exp: "7d",
+    })
+      .guard({}, (app) =>
+        app.derive(async ({ jwt, headers, set }) => {
+          const auth = headers.authorization;
+
+          const token = auth?.startsWith("Bearer ") ? auth.slice(7) : null;
+          if (!token) {
+            set.status = 401;
+            throw new Error("Немає токена");
+          }
+
+          const payload = await jwt.verify(token);
+          if (!payload) {
+            set.status = 401;
+            throw new Error("Невірний токен");
+          }
+
+          console.log("токен пройдений");
+
+          return { userId: payload.sub, role: payload.role };
+        }),
+      )
+      .get("/secured", ({ some }) => {
+        return some;
+      }),
+  )
+  .listen(3000);
+console.log("Сервер на http://localhost:3000");
